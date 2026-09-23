049947af9f34c478d2be2288c7a8419b2567c6c9
Wolyo <144167357+Kaio-Emidio@users.noreply.github.com>
2026-09-23 17:53:04 -0300

    modelo de usuario

--- app/models/usuario.py
+++ app/models/usuario.py
@@ -1,2 +1,16 @@
 from app import db
 
+class Usuario(db.Model):
+    id = db.Column(db.Integer,
+                   primary_key=True)
+    username = db.Column(db.String(65),
+                         index=True,
+                         unique=True,
+                         nullable=False)
+    password_hash = db.Column(db.String(256))
+    email = db.Column(db.String(65),
+                      index=True,
+                      unique=True,
+                      nullable=False)
+    personagens = db.relationship('Personagem',
+                                  back_populates='id_usuario')
